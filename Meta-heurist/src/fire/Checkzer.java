f708c684870ab8086f47d5c898b9b864c7e435fc
M. Rahmoun Mehdi <rahmoun@insa-08130>
2019-05-10 15:03:25 +0200

    Checker à changer

---
+++ Meta-heurist/src/fire/Checkzer.java
@@ -0,0 +1,100 @@
+package fire;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+public class Checkzer {
+	
+	public Checkzer() {
+		
+	}
+	
+	public static int check_chemin(int node_id,String test,Donnees donnee) {
+		int i=-1;
+		int[][] tab_chemins = donnee.getTab_chemins();
+		String[] mots = test.split(" ") ;
+	
+		for (int t=0; t <10; t++) {
+			
+			
+			System.out.println("Test de la ligne n° "+t) ; 
+			System.out.println("Comparaison entre "+tab_chemins[t][0]+" et "+node_id) ; 
+
+			if (tab_chemins[t][0] == node_id){
+				System.out.println("je suis dans le if");
+				int c= 0 ;
+				int taille_mots =mots.length-1;
+				while(tab_chemins[t][c+4]== Integer.parseInt(mots[c]) && c<taille_mots) {
+					c ++ ;
+					System.out.println("Comparaison entre "+tab_chemins[t][c+4]+" et "+mots[c]) ; 
+
+				}
+				
+				//Si toutes les valeurs ont matché jusqu'à la dernière case
+				if (c==taille_mots) {
+					i=t ;
+				}
+			}
+		}
+		return i;
+
+	}
+	
+	public static int check_solution(int node_id,String test,String fichiertest) throws IOException {
+		//Donnees donnee = new Donnees(fichiertest);
+		/*BufferedReader br = new BufferedReader(new FileReader("/home/rahmoun/4IR/Meta-heuristique/meta-heuristique/Test_solutions"+fichiertest));
+		
+		
+		String line;
+		String file = null;
+		int cpt=0;
+		while ((line = br.readLine()) != null) {
+			if(cpt==0) {
+				file=line;
+			}
+			cpt++;
+		}*/
+		//Donnees donnee = new Donnees(file+"_I.full");
+		Donnees donnee = new Donnees("dense_10_30_3_1_I.full") ;
+		int i=-1;
+		int[][] tab_chemins = donnee.getTab_chemins();
+		String[] mots = test.split(" ") ;
+		
+		
+	
+		for (int t=0; t <10; t++) {
+			
+			
+			System.out.println("Test de la ligne n° "+t) ; 
+			System.out.println("Comparaison entre "+tab_chemins[t][0]+" et "+node_id) ; 
+
+			if (tab_chemins[t][0] == node_id){
+				System.out.println("je suis dans le if");
+				int c= 0 ;
+				int taille_mots =mots.length-1;
+				while(tab_chemins[t][c+4]== Integer.parseInt(mots[c]) && c<taille_mots) {
+					c ++ ;
+					System.out.println("Comparaison entre "+tab_chemins[t][c+4]+" et "+mots[c]) ; 
+
+				}
+				
+				//Si toutes les valeurs ont matché jusqu'à la dernière case
+				if (c==taille_mots) {
+					i=t ;
+				}
+			}
+		}
+		return i;
+
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println(check_solution(323,"333 337 342 498 0 520 209 216 473 161 166 170 211 250 281 310 328 336 346 373","dense_10_30_3_8"));
+		
+		
+	}
+	
+	
+}
--- Meta-heurist/src/fire/Donnees.java
+++ Meta-heurist/src/fire/Donnees.java
@@ -7,60 +7,102 @@ import java.io.IOException;
 //Chaz
 ///home/rahmoun/4IR/Meta-heuristique/meta-heuristique/Instances
 public class Donnees {
-private static String file;
-private static int nbevac; //nombre de sommets à évacuer
-private static int noeud_safe; //noeud à atteindre
-private static int compteur_ligne=0;
-private static int tab [][] = new int [10][200];
+private String file;
+private int nbevac; //nombre de sommets à évacuer
+private int noeud_safe; //noeud à atteindre
 
-	/*public Donnees() {
-		file="/home/rahmoun/4IR/Meta-heuristique/meta-heuristique/Instances/dense_10_30_3_1.full";
-	}*/
+private int nb_noeuds_tot; //Nb noeuds
+private int nb_arcs_tot ; //Nb arcs
+
+private int compteur_ligne=0;
+private int[][] tab_chemins = new int [10][200];
+private int[][] tab_arcs = new int [1000][5];
+
+
+	public Donnees(String file1) throws IOException {
+		file="/home/rahmoun/4IR/Meta-heuristique/meta-heuristique/Instances/"+file1;
+		crea_tableaux();
+		
+	}
 	
 	//Ouverture du fichier
-	public static void main(String[] args) throws IOException {
+	public void crea_tableaux() throws IOException {
 		try {
 			
-			BufferedReader br = new BufferedReader(new FileReader("/home/rahmoun/4IR/Meta-heuristique/meta-heuristique/Instances/dense_10_30_3_1.full"));
+			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
 			
 			while ((line = br.readLine()) != null) {
 			   compteur_ligne ++;
 
 
-			   
+			   //1ere ligne info
 			   if(compteur_ligne==2) {
 			    	String[] mots = line.split(" ") ;
 			    	nbevac = Integer.parseInt(mots[0]);
 			    	noeud_safe = Integer.parseInt(mots[1]);
 			
 			    	//Affichage de l'en-tete
-					System.out.println("Nom de l'instance résolue : "+nbevac) ; 
-					System.out.println("Nombre de sommets à évacuer : "+noeud_safe) ; 
+					//System.out.println("Nom de l'instance résolue : "+nbevac) ; 
+					//System.out.println("Nombre de sommets à évacuer : "+noeud_safe) ; 
 
 			    }
 			   
+			   //Chemins jusqu'aux noeuds finaux
 			   if(compteur_ligne>2 && compteur_ligne<13) {
 			    	String[] mots = line.split(" ") ;
 			    	int nb_noeuds = Integer.parseInt(mots[3]) ;
-			    	System.out.println("noeud : "+mots[0]+ " population : "+mots[1]+"; taux max "+mots[2]+"; nombre de noeuds à parcourir : "+mots[3]+"; noeud S : "+mots[nb_noeuds+3]); 
-			    	tab[compteur_ligne-3][0]=Integer.parseInt(mots[0]) ;
-			    	tab[compteur_ligne-3][1]=Integer.parseInt(mots[1]) ;
-			    	tab[compteur_ligne-3][2]=Integer.parseInt(mots[2]) ;
-			    	tab[compteur_ligne-3][3]=Integer.parseInt(mots[3]) ;
+			    	//System.out.println("noeud : "+mots[0]+ " population : "+mots[1]+"; taux max "+mots[2]+"; nombre de noeuds à parcourir : "+mots[3]+"; noeud S : "+mots[nb_noeuds+3]); 
+			    	tab_chemins[compteur_ligne-3][0]=Integer.parseInt(mots[0]) ;
+			    	tab_chemins[compteur_ligne-3][1]=Integer.parseInt(mots[1]) ;
+			    	tab_chemins[compteur_ligne-3][2]=Integer.parseInt(mots[2]) ;
+			    	tab_chemins[compteur_ligne-3][3]=Integer.parseInt(mots[3]) ;
 			    	for (int i=0; i<nb_noeuds; i++) {
-				    	tab[compteur_ligne-3][i+4]=Integer.parseInt(mots[i+4]) ;
+			    		tab_chemins[compteur_ligne-3][i+4]=Integer.parseInt(mots[i+4]) ;
 				    		    		
 			    	}
 
+			    	//print le chemin
 		    		/*System.out.println("chemin: "); 
-			    	for(int i=1; i<(tab[0][3]+1);i++) {
-			    		System.out.print(tab[0][3+i]+" "); //ça print le chemin 
+			    	for(int i=1; i<(tab_chemins[0][3]+1);i++) {
+			    		System.out.print(tab_chemins[0][3+i]+" "); //ça print le chemin 
 			    	}		    	*/
 				   
 			   }
 			   
+			   //Nb noeuds, nb arcs
 			   if(compteur_ligne == 14) {
+			    	String[] mots= line.split(" ") ;
+			    	nb_noeuds_tot = Integer.parseInt(mots[0]);
+			    	nb_arcs_tot = Integer.parseInt(mots[1]);
+			
+			    	//Affichage de l'en-tete
+					//System.out.println("Nb noeuds: "+nb_noeuds_tot) ; 
+					//System.out.println("Nb arcs : "+nb_arcs_tot) ; 
+			   }
+			   
+			 //Chemins jusqu'aux noeuds finaux
+			   if(compteur_ligne>14 ) {
+			    	String[] mots = line.split(" ") ;
+			    	int nb_noeuds = Integer.parseInt(mots[3]) ;
+			    	//System.out.println("noeud n°1 : "+mots[0]+ " noeud n°2 : "+mots[1]+"; duedate "+mots[2]+"; length : "+mots[3]+"; capacity : "+mots[4]); 
+			    	tab_arcs[compteur_ligne-15][0]=Integer.parseInt(mots[0]) ;
+			    	tab_arcs[compteur_ligne-15][1]=Integer.parseInt(mots[1]) ;
+			    	if (mots[2].equals("9223372036854775807")) {
+			    		tab_arcs[compteur_ligne-15][2] = Integer.MAX_VALUE; 
+			    	}
+			    	else {
+			    		tab_arcs[compteur_ligne-15][2]=Integer.parseInt(mots[2]) ;
+			    	}
+			    	tab_arcs[compteur_ligne-15][3]=Integer.parseInt(mots[3]) ;
+			    	tab_arcs[compteur_ligne-15][4]=Integer.parseInt(mots[4]) ;
+
+
+			    	//print le chemin
+		    		/*System.out.println("chemin: "); 
+			    	for(int i=1; i<(tab_chemins[0][3]+1);i++) {
+			    		System.out.print(tab_chemins[0][3+i]+" "); //ça print le chemin 
+			    	}		    	*/
 				   
 			   }
 				   				
@@ -70,10 +112,16 @@ private static int tab [][] = new int [10][200];
 			br.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			System.out.println(file);
-			e.printStackTrace();
+			//System.out.println("Tab arcs : "+tab_arcs);
+			//e.printStackTrace();
 		}
 	
 
 	}
+
+	public int[][] getTab_chemins() {
+		return tab_chemins;
+	}
+
+
 }
