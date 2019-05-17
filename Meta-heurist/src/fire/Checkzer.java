5693e334c6b39944773101d81f8c9df0fb67f2a4
M. Rahmoun Mehdi <rahmoun@insa-10577>
2019-05-17 14:48:00 +0200

    avancement chercker

--- Meta-heurist/src/fire/Checkzer.java
+++ Meta-heurist/src/fire/Checkzer.java
@@ -1,16 +1,19 @@
 package fire;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 public class Checkzer {
 	
+	private ArrayList<Integer> noeudsTest = new ArrayList <>() ;
+
 	public Checkzer() {
-		
+	
 	}
 	
-	public static int check_chemin(int node_id,String test,Donnees donnee) {
+	public int check_chemin(int node_id,String test,Donnees donnee) {
 		int i=-1;
 		int[][] tab_chemins = donnee.getTab_chemins();
 		String[] mots = test.split(" ") ;
@@ -41,23 +44,83 @@ public class Checkzer {
 
 	}
 	
-	public static int check_solution(int node_id,String test,String fichiertest) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("./../../meta-heuristique/Test_solutions/"+fichiertest));
+	public  int check_solution(int node_id,String fichiertest) throws IOException {
+
+		int i=1;	//Si solution non valide, elle devient -1
+		int nb_noeudsTest=0 ;
+
 		
+		//Lire fichier solution
+		BufferedReader br = new BufferedReader(new FileReader("./../../meta-heuristique/Test_solutions/"+fichiertest));
 		
 		String line;
 		String file = null;
-		//int cpt=0;
-		if ((line = br.readLine()) != null) {
+		
+		//Recupere 1ère ligne <==> fichier contenant les données
+		int compteur_ligne=0;
+		if((line = br.readLine())!=null) {
+			compteur_ligne ++;
 			file=line;
 		}
+		
+		//Ouverture et lecture du fichier contenant les données
 		Donnees donnee = new Donnees(file);
-		int i=-1;
 		int[][] tab_chemins = donnee.getTab_chemins();
-		String[] mots = test.split(" ") ;
+		int[][] tab_arcsChecker = donnee.getTab_arcs();
+
+
+
+		while ((line = br.readLine()) != null) {
+			   compteur_ligne ++;
+			   
+			   if(compteur_ligne==2) { 		//Contient le nb de noeuds de départ
+					nb_noeudsTest = Integer.parseInt(line);
+			   }	
+			   
+			   if(2<compteur_ligne && compteur_ligne<(nb_noeudsTest+2)) {
+			    	String[] mots = line.split(" ") ;	 //Exp: 233 1354 0
+			    	//Par défaut tous les noeuds ont un chemin
+			    	noeudsTest.add(Integer.parseInt(mots[0]));	//Noeud initial, figé
+			    	noeudsTest.add(Integer.parseInt(mots[0]));	//Progression, noeud courant
+			    	noeudsTest.add(Integer.parseInt(mots[1]));	//Taux de pop
+			    	noeudsTest.add(Integer.parseInt(mots[2]));	//Heure de début
+			   }
+		}
+				
+		//Vérification
 		
-		
+		int h=0 ; 		//Horloge
+		for (int index_a=0; index_a < nb_noeudsTest ; index_a ++) {
+			int noeud_init = codearraylist(index_a,0) ;
+			int noeud_courant = codearraylist(index_a,1) ;
+			int noeud_next = donnee.next_node(noeud_init, noeud_courant) ;
+			int capa_arc = donnee.get_capaArc(noeud_courant,noeud_next,tab_arcsChecker) ;	//La capa max de l'arc
+			int taux_noeudInit = codearraylist(index_a,2) ; //taux du noeud initial
+			
+			//Comparaison entre le taux du noeud initial et celui de l'arc
+			if (taux_noeudInit < capa_arc) {	//Le transport pt avoir lieu
+				int difference = capa_arc - taux_noeudInit ; //Ce que l'on doit soustraire
+				donnee.set_capaArc(noeud_courant, noeud_next, tab_arcsChecker, difference) ; //Maj du poids de l'arc qui devient occupé mnt
+				
+				//Test si on doit libérer un arc déja occupé auparavant
+				if (codearraylist(index_a,0) != codearraylist(index_a,1)) {		//Donc si le noeud n'est plus dans son état initial (comme ds le fichier test)
+					int noeud_prec = donnee.prec_node(noeud_init, noeud_courant) ;
+					donnee.set_capaArc(noeud_prec, noeud_courant, tab_arcsChecker, capa_arc+taux_noeudInit) ; //On rend à l'arc précédent le taux qui a été utilisé
+				}
+				 //Maj du noeud courant de ce chemin suivi, il avance après av occupé le noeud suivant
+				noeudsTest.set(index_a*4+1, noeud_next) ; 
 	
+			}
+			// [TO BE DONE : Le cas où il n'y a pas assez de capa ds l'arc  + Arret du for
+				// Prendre en compte le tps de début des noeudsS + tps propag arcs
+			
+			
+			
+		}
+		
+
+		
+		/*
 		for (int t=0; t <10; t++) {
 			
 			
@@ -79,17 +142,42 @@ public class Checkzer {
 					i=t ;
 				}
 			}
-		}
+		}*/
 		return i;
 
 	}
 	
+	//[TO BE DONE]
+	public void write_solution() {
+		//invalid
+		//inf
+		//10.5
+		//handmade 0.1.0
+	}
+	
+	
+	/*DANS LA LISTE NOEUDTEST : SI ON VEUT UTILISER LE NOEUD I, IL FAUT ECRIRE
+	 * noeudsTest(i*3)
+	 * 
+	 * SI ON VEUT LE TAUX D'EVACUATION noeudsTest(i*4+2)
+	 * SI ON VEUT LE TEMPS DE DEPART : noeudsTest(i*4+3)
+	 * ON CREE UNE FONCTION PRENANT 2 PARAMETRES : i : LE NUMERO DU NOEUD
+	 * ET j : VALANT 0,1, 2 OU 3 pour savoir si on veut le noeud initial, le noeud courant, le taux d'evacuation
+	 * ou la date de debut d'evacuation  
+	 */
+	public int codearraylist(int index,int code) {
+		return noeudsTest.get(index*4+code);
+	}
 	
 	public static void main(String[] args) throws IOException {
-		System.out.println(check_solution(323,"333 337 342 498 0 520 209 216 473 161 166 170 211 250 281 310 328 336 346 373","dense_10_30_3_8"));
+		Checkzer chaz = new Checkzer();
+		System.out.println(chaz.check_solution(493,"dense_10_30_3_8"));
 		
 		
 	}
+	
+	
+	
 	
 	
 }
--- Meta-heurist/src/fire/Donnees.java
+++ Meta-heurist/src/fire/Donnees.java
@@ -7,16 +7,16 @@ import java.io.IOException;
 //Chaz
 ///home/rahmoun/4IR/Meta-heuristique/meta-heuristique/Instances
 public class Donnees {
-private String file;
-private int nbevac; //nombre de sommets à évacuer
-private int noeud_safe; //noeud à atteindre
-
-private int nb_noeuds_tot; //Nb noeuds
-private int nb_arcs_tot ; //Nb arcs
-
-private int compteur_ligne=0;
-private int[][] tab_chemins = new int [10][200];
-private int[][] tab_arcs = new int [1000][5];
+	private String file;
+	private int nbevac; //nombre de sommets à évacuer
+	private int noeud_safe; //noeud à atteindre
+	
+	private int nb_noeuds_tot; //Nb noeuds
+	private int nb_arcs_tot ; //Nb arcs
+	
+	private int compteur_ligne=0;
+	private int[][] tab_chemins = new int [10][200];
+	private int[][] tab_arcs = new int [1000][5];
 
 
 	public Donnees(String file1) throws IOException {
@@ -122,6 +122,73 @@ private int[][] tab_arcs = new int [1000][5];
 	public int[][] getTab_chemins() {
 		return tab_chemins;
 	}
+	public int[][] getTab_arcs() {
+		return tab_arcs;
+	}
 
+	public int get_capaArc(int node1, int node2, int[][] tab) {
+		
+		//Chercher l'index dans le tab_arcs tel que les 2 noeuds correspondent + Retourner capa
+		int index_i=0 ;
+		while (((tab[index_i][0] != node1) || (tab[index_i][1] != node2)) && index_i<1000) {
+			index_i ++ ;
+		}
+		return tab[index_i][4] ;
+	}
+	
+	public int get_indexArc(int node1, int node2) {
+		
+		//Chercher l'index dans le tab_arcs tel que les 2 noeuds correspondent
+		int index_i=0 ;
+		while (((tab_arcs[index_i][0] != node1) || (tab_arcs[index_i][1] != node2)) && index_i<1000) {
+			index_i ++ ;
+		}
+		return index_i ;
+	}
+	
+	public void set_capaArc(int node1, int node2, int[][] tab, int new_capa) {
+		int index = get_indexArc(node1, node2) ;
+		tab[index][4]=new_capa ;
+	}
+	
+	
+	public int next_node(int noeud_init,int noeud_courant) {
+		
+		int index_i=get_index_fn(noeud_init) ;	//Recup la ligne du chemin à regarder
+		int index_j = 1 ;
+		while (tab_chemins[index_i][index_j] != noeud_courant) {		//Chercher le noeud courant ds le chemin
+			index_j ++ ;
+		}
+		return tab_chemins[index_i][index_j+1];	//On s'interesse au noeud juste après le noeud courant 
+		
+			
+	}
+	
+	public int prec_node(int noeud_init,int noeud_courant) {
+		
+		int index_i=get_index_fn(noeud_init) ;	//Recup la ligne du chemin à regarder
+		int index_j = 1 ;
+		while (tab_chemins[index_i][index_j] != noeud_courant) {		//Chercher le noeud courant ds le chemin
+			index_j ++ ;
+		}
+		return tab_chemins[index_i][index_j-1];	//On s'interesse au noeud juste après le noeud courant 
+		
+			
+	}
+	
+	public int get_index_fn(int s) {		//Get index of tab_chemins from the initial node
+		
+		int index=0 ;
+		while (tab_chemins[index][0] != s) {	// Tq ne l'a pas trouvé
+			index ++ ;
+		}	
+		//CHECK : ERROR CASE, meme si on est sur que tt les noeuds initiaux ont un chemin
+		return index ;
+			
+	}
+	
+	public int get_finalNode() {
+		return noeud_safe ;
+	}
 
 }
